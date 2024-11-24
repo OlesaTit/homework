2bf98936879168120a648a6942269710d93c86c2
Lika <titovaolesy1989@gmail.com>
2024-11-24 17:17:13 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# "1st program"
+
+print(5 * 9**0.5)
+
+# "2nd program"
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# "3rd program"
+
+a = (2 * 2 + 2)
+b = (2 * (2 + 2))
+print(a)
+print(b)
+print(a == b)
+
+# "4th program"
+
+print(float('123.456')* 10)
+print(int(1234.56)% 10)
